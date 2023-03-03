d27813e9462430090a6b5a404d45702d36686a15
Weshy Joe <josephwaweru96@gmail.com>
2023-03-03 14:09:11 +0300

    serializers created

--- wezacare/views.py
+++ wezacare/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.shortcuts import get_object_or_404
+
+from .models import Question, Answer
+from .serilaizers import QuestionSerializer, AnswerSerializer, AnswerUpdateSerializer
 
 # Create your views here.
