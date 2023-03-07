d8177f6ed6a0920c5b9015ae33529b83f0caea44
Weshy Joe <josephwaweru96@gmail.com>
2023-03-07 11:24:39 +0300

    db configs

--- core/settings.py
+++ core/settings.py
@@ -89,10 +89,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-    'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
-
 
 
 # Password validation
@@ -132,10 +131,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
